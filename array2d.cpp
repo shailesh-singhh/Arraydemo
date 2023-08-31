1697dc09358ab8afb72900e2d300885899ff6d5b
Shailesh singh <singhshailesh2901@gmail.com>
2023-08-31 20:50:29 +0530

    Hello

---
+++ array2d.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int row,column;
+    cout<<"Enter the number of rows: "<<endl;
+    cin>>row;
+    cout<<"Enter the number of columns :"<<endl;
+    return 0;
+}
